283a0909bcdefc31d6150ca5448607382ea845bf
Jer <jb14vp@brocku.ca>
2018-11-08 22:09:16 -0500

    Lab 9

---
+++ fixme2.js
@@ -0,0 +1,16 @@
+//Sets the initial colours of the three elements on page load
+function defaultColour(){
+
+	document.getElementById("one").style.color = "Blue";
+	document.getElementById("two").style.color = "Green";
+	document.getElementById("three").style.color = "Red";
+}
+
+
+//A function to change colours of elements when called
+function changeElements(){
+
+	document.getElementById("one").style.color = "#00BB00";
+	document.getElementById("two").style.color = "#BB9320";
+	document.getElementById("three").style.color = "#AB00FF";
+}
